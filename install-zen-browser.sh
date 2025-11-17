4ce0409482866e731e1e86db0511d81d09f36b0e
nzkritik <sg1.kritik@gmail.com>
2025-11-18 10:37:46 +1300

    Comment out zen-browser extension installations
    Comment out installation of uBlock Origin and Bitwarden extensions.

--- install-zen-browser.sh
+++ install-zen-browser.sh
@@ -2,5 +2,5 @@
 
 # Install zen-browser + extensions
 yay -S --noconfirm --needed zen-browser-bin
-yay -S --noconfirm --needed zen-browser-ublock-origin 
-yay -S --noconfirm --needed zen-browser-bitwarden
+# yay -S --noconfirm --needed zen-browser-ublock-origin 
+# yay -S --noconfirm --needed zen-browser-bitwarden
