3cd1b8f5495a01961b02623bf2c14bc8d5183f40
nzkritik <sg1.kritik@gmail.com>
2025-12-02 21:48:29 +1300

    removed fluxgym from ai tools

--- bin/ai-tools.sh
+++ bin/ai-tools.sh
@@ -21,7 +21,7 @@ show_ai_tool_menu() {
         "Open WebUI|Run LLMs with a web interface - CPU/GPU Required|./bin/aitools/install-open-webui.sh|false" #tested
         "Automatic1111|Stable Diffusion Web Interface - GPU Required|./bin/aitools/install-automatic1111.sh|false" #tested
         "ComfyUI|Stable Diffusion Web Interface - GPU Required|./bin/aitools/install-comfyui.sh|false"
-        "FluxGym|LoRA training UI - GPU Recommended|./bin/aitools/install-fluxgym.sh|false" #tested
+        # "FluxGym|LoRA training UI - GPU Recommended|./bin/aitools/install-fluxgym.sh|false" #tested
         "AI Toolkit| All in one training suite for diffusion models|./bin/aitools/install-ai-toolkit.sh|true" #tested
         "Clara Verse|AI Image Generation Tool - GPU Required|./bin/aitools/install-clara-verse.sh|false" #tested
     )
